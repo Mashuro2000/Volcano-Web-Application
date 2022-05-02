e536179945d0b7846fdf1b16a68297c95343cb07
Mashuro2000 <martinmashuro@gmail.com>
2022-05-03 03:56:20 +1000

    work on quering data

--- src/Pages/VolcanoList.js
+++ src/Pages/VolcanoList.js
@@ -1,9 +1,37 @@
-
+import { AgGridReact } from "ag-grid-react";
 import react from "react";
-export default function VolcanoList() {
+import { useState, useEffect } from "react";
+
+export default function VolcanoList(props) {
+  const columns = [
+    { headerName: "Name", field: "name" },
+    { headerName: "Region", field: "region" },
+    { headerName: "Subregion", field: "subregion" },
+  ];  
     return (
       <div className="App">
         <h1>Volcano List</h1>
+
       </div>
     );
   }
+
+function quereyCountrydata(country, radius){
+  const [rowData, setRowData] = useState([]);
+  useEffect(() => {
+    fetch("http://sefdb02.qut.edu.au:3001/volcanoes?country=${country}&populatedWithin=${radius}")
+      .then((res) => res.json())
+      .then((data) => data.works)
+      .then((works) =>
+        works.map((work) => {
+          return{
+            name: work.name,
+            country: work.country,
+            region: work.region,
+            subregion: work.subregion
+          };
+        })
+      )
+      .then((rowData) => setRowData(rowData));
+  }, []);
+}
